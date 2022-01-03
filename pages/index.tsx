5b5aed58fab1cbcbdf6b6906f5ea3df5a82d4892
Matt <mqunell@gmail.com>
2022-01-03 00:33:41 -0800

    begin styling game links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import Image from 'next/image';
 import { GetStaticProps } from 'next';
 import { Game, getGames } from '../lib/games';
 
@@ -10,24 +11,32 @@ export const getStaticProps: GetStaticProps = async () => {
 
 export default function Home({ games }) {
 	return (
-		<div className="flex flex-col gap-2 p-8">
+		<div className="flex flex-col gap-4 p-6">
 			<Head>
 				<title>Next.js App</title>
 				<meta name="description" content="Next.js App" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<h1 className="text-lg underline">Games</h1>
-			<ul>
-				{games &&
-					games.map((game: Game) => (
-						<li key={game.gameId}>
-							<Link href={`/games/${game.gameId}`}>
-								<a>{game.name}</a>
-							</Link>
-						</li>
-					))}
-			</ul>
+			<h1 className="mx-auto text-2xl">Games</h1>
+			<div className="flex flex-wrap justify-center gap-4">
+				{games && games.map((game: Game) => <GameLink key={game.gameId} game={game} />)}
+			</div>
 		</div>
+	);
+}
+
+function GameLink({ game }: { game: Game }) {
+	const { gameId, name, playtimeRecent, playtimeTotal, iconUrl, logoUrl } = game;
+
+	return (
+		<Link href={`/games/${gameId}`}>
+			<a>
+				<div className="flex flex-col items-center w-[150px] px-4 py-4 gap-2 text-black text-center bg-white rounded cursor-pointer">
+					<Image src={iconUrl} alt={`${name} icon`} width={32} height={32} />
+					<h2 className="max-w-full text-lg truncate">{name}</h2>
+				</div>
+			</a>
+		</Link>
 	);
 }
