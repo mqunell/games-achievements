f49ed6bfc875c474a247a9f91ec4df3164c0d43d
Matt <mqunell@gmail.com>
2022-02-27 12:03:08 -0800

    remove animations to begin overhauling

--- components/AchievementCard.tsx
+++ components/AchievementCard.tsx
@@ -22,60 +22,19 @@ export default function AchievementCard({
 }: AchievementCardProps) {
 	const { name, description, completed, completedTime, globalCompleted } = achievement;
 
-	// The HTML node
-	const domRef = useRef<HTMLDivElement>();
-
-	// Scrolled into view
-	const [isVisible, setVisible] = useState(false);
-
-	// Filtered in
-	const initialHeight = useRef<number>();
-	const [isFiltered, setFiltered] = useState(true);
-
-	// Store the initial (full) height and create/attach the observer
-	useEffect(() => {
-		const node = domRef.current;
-
-		initialHeight.current = node.clientHeight;
-
-		const observer = new IntersectionObserver((entries) => {
-			entries.forEach((entry) => {
-				if (entry.isIntersecting) {
-					setVisible(entry.isIntersecting);
-				}
-			});
-		});
-
-		observer.observe(node);
-		return () => observer.unobserve(node);
-	}, []);
-
 	// Call setFiltered() when the filters prop changes to cause a component re-render
+	const [isFiltered, setFiltered] = useState(true);
 	useEffect(() => {
 		setFiltered(filters[completed ? 'showCompleted' : 'showUncompleted']);
 	}, [filters, completed]);
 
+	/* Container - Card and Checkmark sit in the same grid cell */
 	return (
-		/* Container - Card and Checkmark sit in the same grid cell */
-		<div
-			className={classNames(
-				`grid h-[${initialHeight.current} px] mb-8`,
-				'translate-y-8 opacity-0 transition-[height_opacity_translate] duration-500',
-				{ '!translate-y-0 opacity-100': isVisible },
-				{ '!mb-0 h-0 overflow-hidden': !isFiltered }
-			)}
-			ref={domRef}
-		>
+		<div className={classNames('mb-8 grid', !isFiltered && 'hidden')}>
 			{/* Checkmark overlay */}
 			{completed && (
-				<div className={classNames('relative col-start-1 row-start-1')}>
-					<div
-						className={classNames(
-							'absolute -top-3 -right-4 z-10 h-max w-max rounded-full bg-green-500 p-1.5',
-							'scale-0 transition-transform delay-500 duration-500',
-							{ 'scale-100': isVisible }
-						)}
-					>
+				<div className="relative col-start-1 row-start-1">
+					<div className="absolute -top-3 -right-4 z-10 h-max w-max rounded-full bg-green-500 p-1.5">
 						<BadgeCheckIcon className="h-8 w-8 text-white" />
 					</div>
 				</div>
@@ -100,14 +59,7 @@ export default function AchievementCard({
 				{/* Completion bar */}
 				{displayOptions.showGlobal && (
 					<div className="w-full bg-blue-200">
-						<div
-							className={classNames(
-								'bg-blue-600 p-1.5',
-								'origin-left scale-x-0 transition-transform delay-500 duration-500',
-								{ 'scale-x-100': isVisible }
-							)}
-							style={{ width: globalCompleted + '%' }}
-						>
+						<div className="bg-blue-600 p-1.5" style={{ width: globalCompleted + '%' }}>
 							<p className="w-max rounded border border-black bg-white px-1.5 py-0.5 text-xs">
 								{globalCompleted.toFixed(1)}%
 							</p>
