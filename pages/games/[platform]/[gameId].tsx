4da94bd0bd057ab1e41ce4e21217edb2be472696
Matt <mqunell@gmail.com>
2022-03-11 19:18:23 -0800

    add floating back button

--- pages/games/[platform]/[gameId].tsx
+++ pages/games/[platform]/[gameId].tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 import { AnimatePresence, motion } from 'framer-motion';
+import { ArrowLeftIcon } from '@heroicons/react/solid';
 import { Game, Achievement, getGames, getGame } from '../../../lib/games';
 import AchievementCard from '../../../components/AchievementCard';
 import DisplayOptions from '../../../components/DisplayOptions';
@@ -146,6 +148,13 @@ export default function GameAchievements({ game, achievements }: GameAchievement
 					)}
 				</AnimatePresence>
 			</div>
+
+			{/* Floating back button */}
+			<Link href="/">
+				<a className="fixed left-8 bottom-8 rounded border border-black bg-white px-3 py-2 shadow">
+					<ArrowLeftIcon className="h-4 w-4" />
+				</a>
+			</Link>
 		</div>
 	);
 }
